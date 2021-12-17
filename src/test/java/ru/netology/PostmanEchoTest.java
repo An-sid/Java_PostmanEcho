36e1e152daa48317bbdb1cc39fceb0dafa942c1b
An-sid <sidgh@list.ru>
2021-12-17 22:32:14 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.equalTo;
+
+
+class PostmanEchoTest {
+    @Test
+    void shouldMatchRequestBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("foo")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("foo"))
+        ;
+    }
+
+}
